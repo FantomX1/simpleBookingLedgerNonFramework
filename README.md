ad294d4cfa8dcfed4f0c0afa928a9d8e85211fea
frantisek.hallo <fantomx1@gmail.com>
2020-06-17 19:58:49 +0200

    document public root, blackrockdigital/startbootstrap-freelance bootstrap template theme via npm(or yarn) or ( a composer artificial wrapping thought doesnt handle dependencies) issued npm init and then npm i startbootstrap-freelancer

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # simpleBookingLedgerNonFramework
 Application built from components from three frameworks, composer autoloaded
+
+1) issue npm i startbootstrap-freelancer in web folder
+
---
+++ web/index.php
@@ -0,0 +1,23 @@
+<?php
+
+require(__DIR__ . '/../vendor/autoload.php');
+
+ini_set('display_errors', 1);
+error_reporting(E_ALL | E_STRICT);
+
+
+require "../bootstrap.php";
+
+#######################
+$appConfig = [];
+
+$app = new \fantomx1\PhanconX1\App($appConfig);
+
+$app->run();
+
+//var_dump($results);
+
+
+
+
+
